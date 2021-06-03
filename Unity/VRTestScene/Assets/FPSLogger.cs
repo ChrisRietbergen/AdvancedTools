64078e0929ad74dd8e365c1d2c76af9c025b77fb
Chris <C.D.Rietbergen@gmail.com>
2021-06-03 17:30:34 +0200

    Improved FPS logger, made it log every x seconds in which x is a variable settable in the inspector. Default is 10 seconds, may be better to log it every 5 seconds in the future, just a variable for now.

--- Unity/VRTestScene/Assets/FPSLogger.cs
+++ Unity/VRTestScene/Assets/FPSLogger.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class FPSLogger : MonoBehaviour
 {
-	private int averageFPS;
+	[SerializeField]
+	private float loggingInterval = 10.0f;
+
+	private int currentFPS;
 	private TextMeshPro text;
+	private List<int> loggedFPS = new List<int>();
 
 	private void Start()
 	{
 		text = GetComponent<TextMeshPro>();
+		InvokeRepeating("LogAverageFPS", loggingInterval, loggingInterval);
 	}
 
-	public void Update()
+	private void Update()
 	{
-		averageFPS = (int)(1f / Time.unscaledDeltaTime);
-		text.text = averageFPS.ToString() + " FPS";
-		Debug.Log("Current FPS is at " + text.text);
+		currentFPS = (int)(1f / Time.unscaledDeltaTime);
+		text.text = currentFPS.ToString() + " FPS";
+		loggedFPS.Add(currentFPS);
+		//Debug.Log("Current FPS is at " + text.text);
+	}
+
+
+	private void LogAverageFPS()
+	{
+		Debug.Log($"Average FPS for the past {loggingInterval} seconds at : {Time.time.ToString()} seconds in is at {loggedFPS.Average().ToString()} FPS");
+		loggedFPS.Clear();
 	}
 }
 
